86287ba4451c0ee769ce35ae0664cfb6bbd9a25e
Bakhshali <tu78rfrfu@code.edu.az>
2022-05-15 01:19:20 +0400

    Weapon Task add

---
+++ Weekend Day 9/Weekend Day 9/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Weekend_Day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int capacity;
+            int count;
+
+
+            do
+            {
+                Console.WriteLine("Enter Capacity");
+
+                capacity = Convert.ToInt32(Console.ReadLine());
+            } while (capacity<0);
+
+            do
+            {
+                Console.WriteLine("\nEnter Count");
+                count = Convert.ToInt32(Console.ReadLine());
+            } while (count<0 || capacity<count);
+
+            Weapon weapon = new Weapon(capacity, count);
+            bool check = true;
+
+                    do
+                    {
+                        Console.WriteLine("0-Show Information");
+                        int answer = Convert.ToInt32(Console.ReadLine());
+                        switch (answer)
+                        {
+                            case 0:
+                                do
+                                {
+                                    Console.WriteLine("1 - for Shoot Method");
+                                    Console.WriteLine("2 - for Fire Method");
+                                    Console.WriteLine("3 - for GetRemainBulletCount Method");
+                                    Console.WriteLine("4 - for Reload Method");
+                                    Console.WriteLine("5 - for ChangeFireMode Method");
+                                    Console.WriteLine("6 - End");
+
+                                    int answer2 = Convert.ToInt32(Console.ReadLine());
+                                    switch (answer2)
+                                    {
+                                        case 1:
+                                            weapon.Shoot();
+                                            break;
+                                        case 2:
+                                            weapon.Fire();
+                                            break;
+                                        case 3:
+                                            Console.WriteLine(weapon.GetRemainBulletCount());
+                                            break;
+                                        case 4:
+                                            Console.WriteLine(weapon.Reload());
+                                            break;
+                                        case 5:
+                                            weapon.ChangeFireMode();
+                                            break;
+                                        case 6:
+                                                check = false;
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                } while (!check);
+                                break;
+                            default:
+                                break;
+                        }
+
+
+                    } while (check);
+
+                
+            
+        }
+    }
+}
---
+++ Weekend Day 9/Weekend Day 9/Weapon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weekend_Day_9
+{
+    class Weapon
+    {
+        private  int _count;
+
+        public int BulletCapacity { get; set; }
+        public int BulletCount
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                if (value>0 && value<BulletCapacity)
+                {
+                    _count = value;
+                }
+            }
+        }
+        public bool Mood { get; set; }
+
+        public void Shoot()
+        {
+            Console.WriteLine(BulletCount-=1);
+
+        }
+
+        public void Fire()
+        {
+            if (Mood==false)
+            {
+                Console.WriteLine(BulletCount = 0);
+            }
+            
+        }
+       
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity -= BulletCount;
+        }
+
+        public int Reload()
+        {
+            return BulletCapacity = BulletCount + GetRemainBulletCount();
+        }
+
+        public void ChangeFireMode()
+        {
+            if (Mood==true)
+            {
+                Console.WriteLine(false);
+            }
+            else if (Mood == false)
+            {
+                Console.WriteLine(true);
+            }
+        }
+
+        public Weapon(int capacity,int count)
+        {
+            BulletCapacity = capacity;
+            BulletCount = count;
+        }
+
+    }
+}
